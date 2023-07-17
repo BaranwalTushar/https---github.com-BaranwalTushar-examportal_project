53c745438c457cbe6bf4021f73e64692991adaa4
Tushar Baranwal <tusharbaranwal123@gmail.com>
2023-07-18 01:29:34 +0530

    mesage

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UrlSerializer } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  constructor(private userService:UserService) {}
+
+  public user={
+    username:'',
+    password:'',
+    firstname:'',
+    lastname:'',
+    email: '',
+    phone: '',
+
+  };
+
+  ngOnInit(): void {
+      
+  }
+  formSubmit(){
+    console.log(this.user);
+    if(this.user.username=='' || this.user.username==null){
+      alert("User is required!!")
+      return;
+    }
+
+    //addUser: userservice
+    this.userService.addUser(this.user).subscribe({
+      next:(data)=>console.log(data),
+      error:(err)=>console.log(err),  
+      complete:()=>console.log("completed")
+    }
+     
+
+
+    );
+  }
+
+  //
+
 
 }
